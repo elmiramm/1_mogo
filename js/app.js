7caa286aa78ad61350735a33bf992a8d4249dde5
elmiramm <elmira.m.m@yandex.ru>
2022-10-22 15:28:05 +0300

    jqwery - menu nav toggle

--- js/app.js
+++ js/app.js
@@ -41,6 +41,12 @@ $(function () {
 
 	})
 
+	// Menu nav toggle
+	$("#nav-toggle").on("click", function (event) {
+		event.preventDefault();
 
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	});
 
 });
